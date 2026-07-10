e1d75cfa105f2136b15287d917c57df07b361b7d
aarchnemesis <pedro-oliveira-ti@outlook.com>
2026-07-10 07:49:11 -0300

    feat(branding): replace electron icons with official Arthwind Suite logo and apply to window

--- src/main/index.ts
+++ src/main/index.ts
@@ -65,7 +65,7 @@ function createWindow(): void {
     height: 750,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    icon: icon,
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
